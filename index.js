ba72b1e2adb20aa6cc14a7b4946274895b453458
linkel <ironcarbide@gmail.com>
2019-03-08 10:06:17 -0800

    Wrote 3 tests for get and post endpoints using supertest

--- index.js
+++ index.js
@@ -16,4 +16,15 @@ server.get('/games', async (req, res) => {
   res.status(200).json(rows);
 });
 
+server.post('/games', async (req, res) => {
+  const game = req.body
+  if (!game.name || !game.genre) {
+    res.status(422).json({message: "Please include name and genre in req.body"})
+  } else {
+    const send = await games.insert(game)
+    console.log(send)
+    res.status(201).json(send)
+  }
+})
+
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,12 @@
 const request = require('supertest');
 
 const server = require('./index.js');
+const db = require('./data/dbConfig');
 
 describe('index.js testing', () => {
+    beforeEach(async () => {
+        await db('games').truncate();
+    })
     describe('GET /games', () => {
         test('should return 200 OK', async () => {
             const res = await request(server).get('/games');
@@ -15,6 +19,35 @@ describe('index.js testing', () => {
         test('should return empty array if no content', async () => {
             const res = await request(server).get('/games');
             expect(JSON.parse(res.text)).toHaveLength(0);
+        })
+    })
+    describe('POST /games', () => {
+        test('should return the item added if success', async () => {
+            const game = {
+                name: "Smash",
+                genre: "fighting game"
+            }
+            const result = await request(server).post('/games')
+            .send(game)
+            .set('Accept', 'application/json')
+            .expect(201, { 
+                id: 1,
+                name: 'Smash',
+                genre: 'fighting game',
+                releaseYear: null }
+          )
+        })
+        test('should return 422 if incomplete data provided', async () => {
+            const game = {
+                name: "Smash"
+            }
+            const result = await request(server).post('/games')
+            .send(game)
+            .expect(422)
+        })
+        test('should return 422 if no data provided', async () => {
+            const result = await request(server).post('/games')
+            .expect(422)
         })
     })
 })
